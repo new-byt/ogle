f1e5b366915759f4d1d347b77a6bd827080a4ab5
new-byt <55684787+new-byt@users.noreply.github.com>
2021-01-21 20:00:37 +0000

    update

--- Project/public/js/index.js
+++ Project/public/js/index.js
@@ -1,3 +1,92 @@
-//pull category from firebase
+var storageRef = storage.ref();
+//width of page
+var i = 0;
+if (vw <= 768) {
+  if (vw <= 650) {
+    i = 2;
+  } else {
+    i = 1;
+  }
+} else if (vw <= 1024) {
+  i = 1;
+} else {
+  i = 0;
+}
 
-//add films to page
+//Gets films
+let test = async function () {
+  await db
+    .collection("movies")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        i++;
+        if (i <= 4) {
+          createFilmElements(doc.data());
+        }
+      });
+    });
+  pageLoaded();
+};
+test();
+
+//Adds film into to the template
+function createFilmElements(data) {
+  var filmInfo = [
+    ["filmName", data.title],
+    ["filmDescription", data.description],
+    ["filmRating", data.age],
+  ];
+  var filmTemplate = document.querySelector("#filmTemplate");
+  var clone = filmTemplate.content.cloneNode(true);
+  filmInfo.forEach((item) => {
+    console.log(item);
+    var filmData = clone.querySelector("." + item[0]);
+    filmData.textContent = item[1];
+  });
+  //Add trailor image
+  console.log(data.trailer);
+  addTrailorImage(clone, data.trailer);
+  //Add to category
+  var category = document.querySelector(".category");
+  category.querySelector(".films").appendChild(clone);
+}
+
+//Add trailer function
+function addTrailorImage(cloneOfTemplate, trailer) {
+  var trailerElement = cloneOfTemplate.querySelector(".filmPicture");
+  var trailerRef = storageRef.child("trailers/" + trailer);
+  trailerRef
+    .getDownloadURL()
+    .then(function (url) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = "blob";
+      xhr.onload = function (event) {
+        var blob = xhr.response;
+      };
+      xhr.open("GET", url);
+      xhr.send();
+      trailerElement.src = url;
+    })
+    .catch(function (error) {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case "storage/object-not-found":
+          console.log("File not found.");
+          break;
+
+        case "storage/unauthorized":
+          console.log("Unauthorized");
+          break;
+
+        case "storage/canceled":
+          // User canceled the upload
+          break;
+
+        case "storage/unknown":
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
+}
